7aa2d46de8e3538a1a07d2e41c036827b7314d67
sychoi <cinnamon1995@naver.com>
2025-01-14 16:52:29 +0900

    create file

--- 1st_week/pythonprac/hello.py
+++ 1st_week/pythonprac/hello.py
@@ -1,14 +1,17 @@
-def count_fruit(fruit_name):
-    fruits = ['사과','배','배','감','수박','귤','딸기','사과','배','수박']
-    count = 0
+def get_age(name):
+    people = [{'name': 'bob', 'age': 20},
+          {'name': 'carry', 'age': 38},
+          {'name': 'john', 'age': 7},
+          {'name': 'smith', 'age': 17},
+          {'name': 'ben', 'age': 27}]
+    reply = '그런 이름이 없습니다.'
+    for p in people:
+        if p['name'] == name:
+            reply = str(p['age'])
 
-    for f in fruits:
-        if f == fruit_name:
-            count += 1
+    return reply
 
-    return count
 
-print(count_fruit('사과'))
-print(count_fruit('배'))
-print(count_fruit('감'))
-
+print(get_age('bob'))
+print(get_age('john'))
+print(get_age('bk'))
