d876b300254ab84b9519bdb503c5fe8fa1d2b69b
sychoi <cinnamon1995@naver.com>
2025-01-17 21:30:36 +0900

    create file

--- 1st_week/pythonprac/hello.py
+++ 1st_week/pythonprac/hello.py
@@ -2,10 +2,21 @@ from pprint import pprint
 
 import requests # requests 라이브러리 설치 필요
 
-r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
-rjson = r.json()
-rows = rjson['RealtimeCityAir']['row']
-# pprint(rows)
 
-for row in rows:
-    print(row['MSRSTE_NM'], row['IDEX_MVL'])
+def get_gu_mise(name):
+    r = requests.get('http://spartacodingclub.shop/sparta_api/seoulair')
+    rjson = r.json()
+    rows = rjson['RealtimeCityAir']['row']
+    # pprint(rows)
+
+    for row in rows:
+        # print(row['MSRSTE_NM'], row['IDEX_MVL'],row['IDEX_NM'])
+        gu_name = row['MSRSTE_NM']
+        gu_mise = row['IDEX_MVL']
+        if gu_name == name:
+            return gu_mise
+    return "미세먼지 수치가 없습니다."
+
+print(get_gu_mise("중구"))
+print(get_gu_mise("종로구"))
+print(get_gu_mise("수지구"))
