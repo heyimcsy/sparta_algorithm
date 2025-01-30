c3d8810f6eb16adfeb35d8233019fe42e9b37cec
sychoi <cinnamon1995@naver.com>
2025-01-31 00:17:05 +0900

    2주차

--- 2st_week/structures.py
+++ 2st_week/structures.py
@@ -72,62 +72,11 @@ class Queue:
     def is_empty(self):
         return self.front is None
 
-# class HashNode:
-#     def __init(self, key, val, next):
-#         self.key = key
-#         self.val = val
-#         self.next = next
-#
-# class HashTable:
-#     def __init__(self):
-#         self.size = 10
-#         self.table = [None] * self.size
-#
-#     def _hash_function(self, key):
-#         return key % self.size
-#
-#     def put(self, key, val):
-#         idx = self._hash_function(key)
-#
-#         if self[idx] is None:
-#             self.table[idx] = HashNode(key, val, None)
-#         else:
-#             node = self.table[idx]
-#             while node.next is not None:
-#                 node = node.next
-#             node.next = HashNode(key, val, None)
-#
-#     def get(self, key):
-#         idx = self._hash_function(key)
-#         node = self.table[idx]
-#
-#         while node is not None:
-#             if node.key == key:
-#                 return node.val
-#             node = node.next
-#
-#         return -1
-#
-#     def remove(self, key):
-#         idx = self._hash_function(key)
-#         node = self.table[idx]
-#         prev_node = None
-#
-#         while node is not None:
-#             if node.key == key:
-#                 if prev_node is not None:
-#                     prev_node.next = node.next
-#                 else:
-#                     self.table[idx] = node.next
-#             prev_node = node
-#             node = node.next
-
 class HashNode:
-    def __init__(self, key=None, value=None):
+    def __init__(self, key, val, next):
         self.key = key
-        self.value = value
-        self.next = None
-
+        self.val = val
+        self.next = next
 
 class HashTable:
     def __init__(self):
@@ -137,35 +86,130 @@ class HashTable:
     def _hash_function(self, key):
         return key % self.size
 
-    def put(self, key, value):
-        index = self._hash_function(key)
-        if self.table[index] is None:
-            self.table[index] = HashNode(key, value)
+    def put(self, key, val):
+        idx = self._hash_function(key)
+
+        if self.table[idx] is None:
+            self.table[idx] = HashNode(key, val, None)
         else:
-            node = self.table[index]
+            node = self.table[idx]
             while node.next is not None:
                 node = node.next
-            node.next = HashNode(key, value)
+            node.next = HashNode(key, val, None)
 
     def get(self, key):
-        index = self._hash_function(key)
-        node = self.table[index]
+        idx = self._hash_function(key)
+        node = self.table[idx]
+
         while node is not None:
             if node.key == key:
-                return node.value
+                return node.val
             node = node.next
+
         return -1
 
     def remove(self, key):
-        index = self._hash_function(key)
-        node = self.table[index]
+        idx = self._hash_function(key)
+        node = self.table[idx]
         prev_node = None
+
         while node is not None:
             if node.key == key:
-                if prev_node is None:
-                    self.table[index] = node.next
-                else:
+                if prev_node is not None:
                     prev_node.next = node.next
-                return
+                else:
+                    self.table[idx] = node.next
             prev_node = node
             node = node.next
+
+
+class BinaryMaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    def insert(self, val):
+        self.items.append(val)
+
+        cur = len(self.items) - 1
+        parent = cur // 2
+
+        while parent > 0:
+            if self.items[cur] > self.items[parent]:
+                self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+
+            cur = parent
+            parent = cur //2
+
+    def extract(self):
+        if len(self.items) < 2:
+            return None
+
+        root = self.items[1]
+        self.items[1], self.items[-1] =self.items[-1], self.items[1]
+        self.items.pop()
+
+        self._percolate_down(1)
+        return root
+
+    def _percolate_down(self, cur):
+        largest = cur
+        left = 2 * cur
+        right = 2 * cur + 1
+
+        if left < len(self.items) and self.items[left] > self.items[largest]:
+            largest = left
+        if right < len(self.items) and self.items[right] > self.items[largest]:
+            largest = right
+        if largest != cur:
+            self.items[largest], self.items[cur] = self.items[cur], self.items[largest]
+            self._percolate_down(largest)
+
+#
+# class BinaryMaxHeap:
+#     def __init__(self):
+#         # 계산 편의를 위해 0이 아닌 1번째 인덱스부터 사용한다.
+#         self.items = [None]
+#
+#     def insert(self, k):
+#         self.items.append(k)
+#         self._percolate_up()
+#
+#     def _percolate_up(self):
+#         # percolate: 스며들다.
+#         cur = len(self.items) - 1
+#         # left 라면 2*cur, right 라면 2*cur + 1 이므로 parent 는 항상 cur // 2
+#         parent = cur // 2
+#
+#         while parent > 0:
+#             if self.items[cur] > self.items[parent]:
+#                 self.items[cur], self.items[parent] = self.items[parent], self.items[cur]
+#
+#             cur = parent
+#             parent = cur // 2
+#
+#     def extract(self):
+#         if len(self.items) < 2:
+#             return None
+#
+#         root = self.items[1]
+#         self.items[1] = self.items[-1]
+#         self.items.pop()
+#         self._percolate_down(1)
+#
+#         return root
+#
+#     def _percolate_down(self, cur):
+#         biggest = cur
+#         left = 2 * cur
+#         right = 2 * cur + 1
+#
+#         if left <= len(self.items) - 1 and self.items[left] > self.items[biggest]:
+#             biggest = left
+#
+#         if right <= len(self.items) - 1 and self.items[right] > self.items[biggest]:
+#             biggest = right
+#
+#         if biggest != cur:
+#             self.items[cur], self.items[biggest] = self.items[biggest], self.items[cur]
+#             self._percolate_down(biggest)
+
