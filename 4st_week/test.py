7a228a84be2148de6a546fffcc46c598dd088c17
sychoi <cinnamon1995@naver.com>
2025-02-07 23:12:28 +0900

    3주차

--- 4st_week/test.py
+++ 4st_week/test.py
@@ -1,7 +1,10 @@
-from prac import bubblesort, selectionsort
+from prac import bubblesort, selectionsort, insertionsort
 
 assert bubblesort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert bubblesort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
 
 assert selectionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
 assert selectionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
+
+assert insertionsort([4, 6, 2, 9, 1]) == [1, 2, 4, 6, 9]
+assert insertionsort([3, 2, 1, 5, 3, 2, 3]) == [1, 2, 2, 3, 3, 3, 5]
