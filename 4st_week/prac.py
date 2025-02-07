de57373b2d3eac71b630774bc911df5fdf3bb70f
sychoi <cinnamon1995@naver.com>
2025-02-07 23:08:25 +0900

    3주차

---
+++ 4st_week/prac.py
@@ -0,0 +1,24 @@
+def bubblesort(lst):
+    # 최댓값을 구하는 알고리즘을 len(lst) - 1 만큼 반복한다.
+    iters = len(lst) - 1
+    for iter in range(iters):
+        # 이미 구한 최댓값은 범위에서 제외한다.
+        wall = iters - iter
+        for cur in range(wall):
+            if lst[cur] > lst[cur + 1]:
+                lst[cur], lst[cur + 1] = lst[cur + 1], lst[cur]
+    return lst
+
+
+def selectionsort(lst):
+    iters = len(lst) - 1
+    for iter in range(iters):
+        minimun = iter
+        for cur in range(iter + 1, len(lst)):
+            if lst[cur] < lst[minimun]:
+                minimun = cur
+
+        if minimun != iter:
+            lst[minimun], lst[iter] = lst[iter], lst[minimun]
+
+    return lst
